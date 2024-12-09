8ae08db33c2cc32503edd1ec7b0764bfb251b5e2
Caio Cezar <caio_25m@hotmail.com>
2024-12-08 21:50:12 -0300

    Seller entity clss

--- application/Program.java
+++ application/Program.java
@@ -1,13 +1,20 @@
 package application;
 
-import model.entities.Department;
+import java.util.Date;
 
+import model.entities.Department;
+import model.entities.Seller;
+/**programa Principal
+ * 
+ */
 public class Program {
 
 	public static void main(String[] args) {
 	
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmil.com", new Date(), 3000.0, obj);
+		System.out.println(seller);
 		
 	}
 
